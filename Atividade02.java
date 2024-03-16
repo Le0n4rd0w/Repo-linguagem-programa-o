6dcac1277605de72022306c30f5ed37691d15038
Le0n4rd0w <162758364+Le0n4rd0w@users.noreply.github.com>
2024-03-15 21:42:33 -0300

    Create Atividade02.java

---
+++ Atividade02.java
@@ -0,0 +1,15 @@
+package aula01;
+
+public class Aula1 {
+   public static void main (String[] args) {
+   String sexo = "M";
+	String nome = "Leonardo Bertotti";
+   int idade = 33;
+   
+   //imprima no console
+   System.out.println("Sexo: " + sexo);
+   System.out.println("Nome: " + nome);
+   System.out.println("Idade: " + idade);
+   
+   }
+}
