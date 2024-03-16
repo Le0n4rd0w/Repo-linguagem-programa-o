5653d1f01b39194c05af84885e9b90be764d2765
Le0n4rd0w <162758364+Le0n4rd0w@users.noreply.github.com>
2024-03-15 21:43:15 -0300

    Create Atividade03.java

---
+++ Atividade03.java
@@ -0,0 +1,23 @@
+package aula01;
+
+public class aula2 {
+
+	public static void main (String[] args) {
+		String nome = "Lucas";
+		int idade = 33;
+		int batimentoCardiaco = 220 - idade;
+		
+		int cpf = 1112223334;
+		int cep = 1232341;
+		String cidade = "Itabaia";
+		String estado = "Itabiaba";
+		
+		System.out.print("O nome da pessoa é " + nome + ", tem " + idade + " anos e o batimento está " + batimentoCardiaco);
+		
+		   System.out.println("Nome: " + nome);
+		   System.out.println("CPF: " + cpf);
+		   System.out.println("CEP: " + cep);
+		   System.out.println("Cidade: " + cidade);
+		   System.out.println("Estado: " + estado);
+	}
+}
